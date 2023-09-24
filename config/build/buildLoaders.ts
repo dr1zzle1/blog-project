45f2ef3c9dc72df67243f9c3decc86b518107bfc
Valiev Marat <marat.rakhimov.2016@gmail.com>
2023-09-25 00:47:31 +0300

    added Navbar, Sidebar etc.

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,29 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[]{
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[]{
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+    }
 
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
-            "style-loader",
-            "css-loader",
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules:{
+                    auto: (resPath: string) => Boolean(resPath.includes('.module')),
+                    localIdentName: isDev 
+                    ? '[path][name]__[local]--[hash:base64:5]' 
+                    : 'hash:base64:8'
+                    }
+                }
+            },
             "sass-loader"
         ]
     }
@@ -17,5 +34,14 @@ export function buildLoaders(): webpack.RuleSetRule[]{
             exclude: /node_modules/,
     }
 
-    return [typescriptLoader,cssLoader]
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader'
+            },
+        ],
+    }
+
+    return [fileLoader,svgLoader,typescriptLoader,cssLoader]
 }
