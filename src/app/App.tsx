b534c57a61e0f5b4aae122ec66bc3be983ec4fbd
Valiev Marat <marat.rakhimov.2016@gmail.com>
2023-10-19 09:31:33 +0300

    one more fix theme for modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,11 @@ import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
 import { classNames } from '../shared/lib/classNames/classNames';
-import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 
 function App() {
-    const { theme } = useTheme();
-
     return (
-        <div className={classNames('app', {}, [theme])}>
+        <div className={classNames('app', {}, [])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -22,7 +22,6 @@ export const Modal = (props:ModalProps) => {
 
     const [isClosing, setIsClosing] = useState(false);
     const timerRef = useRef < ReturnType<typeof setTimeout>>();
-    const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -60,7 +59,7 @@ export const Modal = (props:ModalProps) => {
     };
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className, theme])}>
+            <div className={classNames(cls.Modal, mods, [className])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content} onClick={onContentClick}>{children}</div>
                 </div>
