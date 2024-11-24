680201bf8480a6bb01e45b6c9ab81ca45c7be984
Marat Valiev <marat.rakhimov.2016@gmail.com>
2024-11-24 13:49:20 +0300

    added Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,6 +8,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { Suspense } from "react";
 import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
 
 function App() {
   const { theme } = useTheme();
@@ -15,7 +16,10 @@ function App() {
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 }
