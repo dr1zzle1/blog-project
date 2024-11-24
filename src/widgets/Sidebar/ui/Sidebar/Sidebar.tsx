66284a5e6593d0ae7c089ee9771a90deaa9c5a49
Marat Valiev <marat.rakhimov.2016@gmail.com>
2024-11-24 14:32:20 +0300

    added i18next

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 
 import cls from "./Sidebar.module.scss";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
   className?: string;
@@ -18,6 +19,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   );
