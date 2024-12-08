49206987c49172e257f1d6ab001b5e52a244183b
Marat Valiev <marat.rakhimov.2016@gmail.com>
2024-12-08 22:10:40 +0300

    added redux store and first entity

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "shared/ui/Button/Button";
+import { counterActions } from "../model/slice/counterSlice";
+import { getCounterValue } from "../model/selectors/getCounterValue/getCounterValue";
+
+interface CounterProps {
+  className?: string;
+}
+
+export const Counter = ({ className }: CounterProps) => {
+  const dispatch = useDispatch();
+  const counterValue = useSelector(getCounterValue);
+  const increment = () => {
+    dispatch(counterActions.increment());
+  };
+  const decrement = () => {
+    dispatch(counterActions.decrement());
+  };
+  return (
+    <div>
+      <h1 data-testid="value-title">{counterValue}</h1>
+      <Button data-testid="increment-btn" onClick={increment}>
+        +
+      </Button>
+      <Button data-testid="decrement-btn" onClick={decrement}>
+        -
+      </Button>
+    </div>
+  );
+};
